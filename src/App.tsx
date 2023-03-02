6de5971cab9dfe1826a4a1114e5498d4060eda2b
Berezin Konstantin <berezinko90@gmail.com>
2023-03-02 20:05:57 +0700

    Верстка по макекту

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import SearchInput from "./components/SearchInput";
 import Table from "./components/Table";
 
 function App() {
+  useEffect(() => {
+    document.title = "new title";
+  }, []);
+
   return (
-    <div className="app">
-      <SearchInput />
-      <Table />
-    </div>
+    <>
+      <div className="app">
+        <SearchInput />
+        <Table />
+      </div>
+    </>
   );
 }
 
